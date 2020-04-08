aec051a7b2933682419b510adea67ace40e132e9
xzq666 <1192227411@qq.com>
2020-04-08 14:37:57 +0800

    首次提交

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AudioAndVideo
+//
+//  Created by qhzc-iMac-02 on 2020/4/8.
+//  Copyright © 2020 Xuzq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
